57c49f7f156737b824dd563b155bd1abcc77dc23
justgitgood <2003habeeb@gmail.com>
2025-02-20 18:08:13 +0000

    Did a bit more changes.

--- headers/user.hpp
+++ headers/user.hpp
@@ -2,10 +2,22 @@
 #define USER_HPP
 
 #include <iostream>
+#include <fstream>
 
+// Client Data
 class client_data {
 public:
+  void set_client_name(std::string client_name);
 private:
   std::string client_name;
+};
+
+
+// Client Data File
+class client_data_file {
+  public:
+    std::ofstream client_profile();
+    void checkfile();
+  private:
 };
 #endif // USER_HPP
---
+++ source/user.cpp
@@ -0,0 +1,12 @@
+#include "../headers/user.hpp"
+
+void client_data::set_client_name(std::string client_name){
+ std::cout << "What would you like your username to be?" << '\n'; 
+ std::cin >> client_name;
+}
+
+void client_data_file::checkfile(){
+  client_data c_client_data;
+  std::ofstream client_profile ("user.txt");
+  client_profile << c_client_data.client_name;
+}
